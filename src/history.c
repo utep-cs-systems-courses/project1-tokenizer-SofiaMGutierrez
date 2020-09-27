668453283636ecbc22b7146dd3f2cf4402ec0ac9
Sofia Gutierrez <sofia.mgutie@gmail.com>
2020-09-27 03:09:02 -0600

    modified all functions

---
+++ src/history.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "history.h"
+
+int main(){
+  List *history = init_history();
+  add_history(history,"i like cake");
+  print_history(history);
+}
+
+List *init_history(){
+  List *history_list = malloc(sizeof(List));
+  history_list->root = malloc(sizeof(Item));
+  return history_list;
+}
+
+void add_history(List *list, char *str){
+  Item *t = list->root; // temp value
+  int count = 0;
+  // Reaches last node
+  while(t->next != NULL){
+    count++;
+    t = t->next;
+  }
+  t->str = str;
+}
+
+char *get_history(List *list, int id){
+}
+
+void print_history(List *list){
+  Item *t = list->root;
+  while(t->next != NULL){
+    printf("str: %s\n", t->str);
+    t = t->next;
+  }
+}
+
+void free_history(List *list){
+  free(list);
+}
